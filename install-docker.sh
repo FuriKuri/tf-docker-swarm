b342b56a62c432c276668af53b03f93672fe4f23
Theo Pack <tf.pack@gmail.com>
2017-12-03 13:59:20 +0100

    Add docker-compose

--- install-docker.sh
+++ install-docker.sh
@@ -19,3 +19,5 @@ add-apt-repository \
 apt-get update
 apt-get install -y docker-ce
 
+curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
